0ffcf877e3833e88b22aac6bf44c0f6ad7a0c9ba
tsukabon <shtsukam@outlook.com>
2021-08-03 11:53:41 +0900

    プロジェクト ファイルを追加します。

---
+++ webapl-cosmos-db/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapl_cosmos_db.Models
+{
+    using Newtonsoft.Json;
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public bool Completed { get; set; }
+    }
+}
---
+++ webapl-cosmos-db/Services/ICosmosDbService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapl_cosmos_db.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using webapl_cosmos_db.Models;
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
